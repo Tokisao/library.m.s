4855375ef4ad5aea2611ef1d4d50bdbc7d69c307
Tokisao <252830@astanait.edu.kz>
2026-02-04 18:38:55 +0500

    Update Book.java

--- src/models/Book.java
+++ src/models/Book.java
@@ -7,17 +7,20 @@ public class Book {
     private String description;
     private int available_copies;
     private int total_copies;
+    private String category;
 
-    public Book(int book_id, String name, String author, String description, int available_copies) {
+    public Book(int book_id, String name, String author, String description, int available_copies, String category) {
         this.book_id = book_id;
         this.name = name;
         this.author=author;
         this.description=description;
         this.available_copies=available_copies;
+        this.category=category;
+        this.total_copies = available_copies;
     }
     @Override
     public String toString() {
-        return "Book name: " + name + "\nAuthor: " + author + "\nDescription: " + description +"\nAvailable copies: " + available_copies;
+        return "\n\nBook ID: " +book_id + "\nBook name: " + name + "\nAuthor: " + author + "\nDescription: " + description +"\nAvailable copies: " + available_copies + "\nCategory: " + category;
     }
 
     public Integer getAvailableCopies(){
@@ -26,5 +29,16 @@ public class Book {
     public Integer getTotalCopies() {
         return total_copies;
     }
-
+    public String getName(){
+        return name;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getCategory(){
+        return category;
+    }
 }
